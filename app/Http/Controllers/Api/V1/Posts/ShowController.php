71188dc56720a8d56549fa925953636d9df60c00
Subash Chaudhary <bcoolboy8@gmail.com>
2022-02-05 13:48:23 +0545

    show route

--- app/Http/Controllers/Api/V1/Posts/ShowController.php
+++ app/Http/Controllers/Api/V1/Posts/ShowController.php
@@ -5,13 +5,27 @@ namespace App\Http\Controllers\Api\V1\Posts;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Posts\PostResource;
 use Domain\Blogging\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use JustSteveKing\StatusCode\Http;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ShowController extends Controller
 {
 
-    public function __invoke(Request $request, Post $post)
+    public function __invoke(Request $request, Post $post): JsonResponse
     {
-        return $post;
+        $post = QueryBuilder::for(
+            subject: $post
+        )->allowedIncludes(
+            includes: ['user'],
+        )->first();
+
+        return response()->json(
+            data: new PostResource(
+                resource: $post,
+            ),
+            status: Http::OK,
+        );
     }
 }
